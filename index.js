fcc8efffd54df1fdee05b6c94d85c80f8e9cc351
lanmower <almagestfraternite@gmail.com>
2023-10-03 02:39:05 +0000

    update

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-require('dotenv').config()
-const runCall = (input)=>{
-  console.log(input)
-  return input
+const OpenAI = require('openai')
+const runCall = async (input) => {
+  const openai = new OpenAI({
+    apiKey: process.env["OPENAI_API_KEY"]
+  });
+  const chatCompletion = await openai.chat.completions.create({
+    messages: input.messages || [{ role: 'user', content: input.question }],
+    model: input.model || 'gpt-3.5-turbo',
+  });
+  console.log(chatCompletion.choices[0]);
+  return chatCompletion.choices[0]
 }
 
 module.exports = runCall
